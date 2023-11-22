d0217b4131731e437daef714fbdaf5ceb386d10d
GiovanniLafortezza <recordeknot388@gmail.com>
2023-11-22 14:40:01 +0100

    step 1

---
+++ js/main.js
@@ -0,0 +1,56 @@
+let automobili = [
+    {
+        marca: "fiat",
+        modello: "2020",
+        alimentazione: "benzina"
+    },
+    {
+        marca: "audi",
+        modello: "2016",
+        alimentazione: "diesel"
+    },
+    {
+        marca: "suzuki",
+        modello: "2018",
+        alimentazione: "gpl"
+    },
+    {
+        marca: "dodge",
+        modello: "2000",
+        alimentazione: "benzina"
+    },
+    {
+        marca: "skyline",
+        modello: "2002",
+        alimentazione: "benzina"
+    },
+    {
+        marca: "bmw",
+        modello: "2019",
+        alimentazione: "elettrica"
+    },
+    {
+        marca: "mecedes",
+        modello: "2009",
+        alimentazione: "ibrida"
+    },
+    {
+        marca: "ferrari",
+        modello: "2006",
+        alimentazione: "diesel"
+    },
+    {
+        marca: "mclaren",
+        modello: "2012",
+        alimentazione: "diesel"
+    },
+    {
+        marca: "lamborghini",
+        modello: "2021",
+        alimentazione: "elettrica"
+    }
+];
+
+let automobiliBenzina = [];
+let automobiliDiesel = [];
+let automobiliRestanti = [];
